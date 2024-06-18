a92e033a8d47308e2841b3fa229d71112ce09e18
Eddahkarambu <gikundaeddah@gmail.com>
2024-06-18 17:35:59 +0300

    omported the use state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 
+import {useState} from "react"
 export default function Home() {
+  const [inputVal, setInputVal] = useState("")
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
-      <form><input type="text" placeholder="Type your name ..."/></form>
-      <button type="submit">Predict Data</button>
+      <form>
+        <input type="text" placeholder="Type your name ..." 
+        value={inputVal}
+        onChange={(e) =>setInputVal(e.target.value)}/>
+        <button type="submit">Predict Data</button>
+        </form>
+      
     </div>
 
   );
