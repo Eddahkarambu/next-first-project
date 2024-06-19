481ecc1ccbd101d56313ab5394c377cf798d1e12
Eddahkarambu <gikundaeddah@gmail.com>
2024-06-19 17:06:24 +0300

    import age data, genderdata and country data

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,8 +1,26 @@
+const getPredictedAge = async (name: string)=>{
+  const res = await fetch(`https://agify.io/?name=${name}`);
+  return res.json()
+}
+
+const getPredictedGender = async (name: string)=>{
+  const res = await fetch(`https://genderize.io/?name=${name}`);
+  return res.json()
+}
+const getPredictedCountry
+ = async (name: string)=>{
+  const res = await fetch(`https://nationalize.io/?name=${name}`);
+  return res.json()
+}
+
 interface Params{
   params:{name:string}
 }
 
-export default function Page({params}:any) {
+export default async function Page({params}:Params) {
+  const ageData = getPredictedAge(params.name)
+  const genderData = getPredictedAge(params.name)
+  const countryData = getPredictedAge(params.name)
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">Name
        {params.name} 
