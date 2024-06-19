a2f274bd2a1f3e470312b7db6862b8fa825708da
Eddahkarambu <gikundaeddah@gmail.com>
2024-06-19 14:16:10 +0300

    add the use state and handle submit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import {useState} from "react"
+import {useState, FormEvent} from "react"
 export default function Home() {
   const [inputVal, setInputVal] = useState("")
+  const handleSubmit = (event: FormEvent) =>{
+    
+  }
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Type your name ..." 
         value={inputVal}
         onChange={(e) =>setInputVal(e.target.value)}/>
